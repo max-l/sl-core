3778dd42f98f8883ab32df021774955697b21bb4
Maxime Lévesque <maxime.levesque@gmail.com>
2012-01-25 10:05:40 -0500

    build settings for oss sonatype

--- project/Build.scala
+++ project/Build.scala
@@ -4,24 +4,48 @@ import Keys._
 
 
 object Buildz extends Build {
-				   
-  
+
+
   def buildSettings = Defaults.defaultSettings ++ Seq(
-    organization := "net.strong_links",
-    version := "0.2",
+    organization := "com.strong-links",
+    version := "0.2-SNAPSHOT",
     scalaVersion := "2.9.1",
-    logLevel in Global := Level.Warn,
-    publishArtifact in packageDoc := false
-  )
-    
-  lazy val buildProject = Project(
-    id = "project",
-    base = file("project"),    
-    settings = buildSettings ++ Seq(    
-      sbtPlugin := true
+    publishMavenStyle := true,
+    publishArtifact in packageDoc := false,
+    pomIncludeRepository := { x => false },
+    publishTo <<= version { (v: String) =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "content/repositories/snapshots") 
+      else                             Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    },
+    credentials += Credentials(Path.userHome / ".ivy2" / "credentials.oss"),
+    pomExtra := (
+      <url>http://github.com/strong-links/core</url>
+      <licenses>
+        <license>
+          <name>Apache 2</name>
+          <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:strong-links/core.git</url>
+        <connection>scm:git:git@github.com:strong-links/core.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>jrlemieux</id>
+          <name>Jacques Lemieux</name>
+        </developer>
+        <developer>
+          <id>max-l</id>
+          <name>Maxime Lévesque</name>
+        </developer>
+      </developers>
     )
-  )  
-  
+  )
+
+
   lazy val core = Project(
     id = "core",
     base = file("core"),
@@ -35,6 +59,9 @@ object Buildz extends Build {
   lazy val root = Project(
     id = "root",
     base = file("."),
-    settings = buildSettings 
+    settings = buildSettings ++ Seq(
+      publish := (),
+      publishLocal := ()
+    )
   ) aggregate(core)    
 }
