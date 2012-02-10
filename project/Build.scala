2bb5a0e4ab715dfc528e367d3b98375251e5e13e
Maxime Lévesque <maxime.levesque@gmail.com>
2012-02-10 11:01:32 -0500

    removed dev environement specific settings

--- project/Build.scala
+++ project/Build.scala
@@ -7,28 +7,12 @@ object Buildz extends Build {
   def buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "com.strong-links",
     version := "0.2",
-    scalaVersion := "2.9.1",
-    logLevel in Global := Level.Warn,
-    publishArtifact in packageDoc := false
+    scalaVersion := "2.9.1"
   )
-
-  lazy val buildProject = Project(
-    id = "project",
-    base = file("project"),    
-    settings = buildSettings ++ Seq(    
-      sbtPlugin := true
-    )
-  )
-
+  
   lazy val core = Project(
     id = "core",
     base = file("core"),
     settings = buildSettings 
   )
-
-  lazy val root = Project(
-    id = "root",
-    base = file("."),
-    settings = buildSettings 
-  ) aggregate(core)    
 }
